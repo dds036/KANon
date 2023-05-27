114562da7f90ee981fcf385b5a962b27eb3e75a3
Denis Fortitude <124372225+dds036@users.noreply.github.com>
2023-05-27 13:58:43 +0300

    Alpha 0.5
    Основные функции программы были дописаны. Один из огромных шагов к великому дню!!!

--- KANon/KANon/Form1.Designer.cs
+++ KANon/KANon/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.пользовательToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -70,9 +71,8 @@
             this.tableAdapterManager = new KANon.KANonDBDataSet1TableAdapters.TableAdapterManager();
             this.сообщенияTableAdapter2 = new KANon.KANonDBDataSetTableAdapters.СообщенияTableAdapter();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.кодDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.текстСообщенияDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.датаОтправкиDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.администраторToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьДиалогToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -89,11 +89,12 @@
             this.файлToolStripMenuItem,
             this.видToolStripMenuItem,
             this.звукToolStripMenuItem,
-            this.справкаToolStripMenuItem});
+            this.справкаToolStripMenuItem,
+            this.администраторToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
             this.menuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
-            this.menuStrip1.Size = new System.Drawing.Size(434, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(1029, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -253,14 +254,14 @@
             // оПрограммеToolStripMenuItem
             // 
             this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
-            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.оПрограммеToolStripMenuItem.Text = "О программе";
             this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.ОПрограммеToolStripMenuItem_Click);
             // 
             // оПредприятииToolStripMenuItem
             // 
             this.оПредприятииToolStripMenuItem.Name = "оПредприятииToolStripMenuItem";
-            this.оПредприятииToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.оПредприятииToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.оПредприятииToolStripMenuItem.Text = "О предприятии";
             this.оПредприятииToolStripMenuItem.Click += new System.EventHandler(this.ОПредприятииToolStripMenuItem_Click);
             // 
@@ -268,7 +269,7 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Silver;
             this.button1.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(341, 610);
+            this.button1.Location = new System.Drawing.Point(875, 610);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(52, 29);
             this.button1.TabIndex = 1;
@@ -284,7 +285,7 @@
             this.textBox1.Location = new System.Drawing.Point(12, 610);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(323, 63);
+            this.textBox1.Size = new System.Drawing.Size(857, 63);
             this.textBox1.TabIndex = 2;
             this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
             // 
@@ -292,7 +293,7 @@
             // 
             this.button2.BackColor = System.Drawing.Color.Silver;
             this.button2.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.Location = new System.Drawing.Point(341, 645);
+            this.button2.Location = new System.Drawing.Point(875, 645);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(52, 28);
             this.button2.TabIndex = 3;
@@ -303,11 +304,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Black;
-            this.label1.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label1.Location = new System.Drawing.Point(0, 24);
+            this.label1.Location = new System.Drawing.Point(12, 27);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(359, 37);
+            this.label1.Size = new System.Drawing.Size(637, 65);
             this.label1.TabIndex = 4;
             this.label1.Text = "KANon (Анонимный чат КАИ)";
             // 
@@ -315,7 +316,7 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(354, 24);
+            this.pictureBox1.Location = new System.Drawing.Point(949, 27);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(80, 67);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -326,7 +327,7 @@
             // 
             this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(399, 644);
+            this.pictureBox2.Location = new System.Drawing.Point(933, 645);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(31, 29);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -337,7 +338,7 @@
             // 
             this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(399, 610);
+            this.pictureBox3.Location = new System.Drawing.Point(933, 610);
             this.pictureBox3.Name = "pictureBox3";
             this.pictureBox3.Size = new System.Drawing.Size(31, 29);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -389,40 +390,36 @@
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.кодDataGridViewTextBoxColumn,
-            this.текстСообщенияDataGridViewTextBoxColumn,
-            this.датаОтправкиDataGridViewTextBoxColumn});
-            this.dataGridView1.DataSource = this.mailsBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(47, 112);
+            this.dataGridView1.Location = new System.Drawing.Point(0, 100);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(346, 320);
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.Transparent;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.Size = new System.Drawing.Size(976, 504);
             this.dataGridView1.TabIndex = 8;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
             // 
-            // кодDataGridViewTextBoxColumn
+            // администраторToolStripMenuItem
             // 
-            this.кодDataGridViewTextBoxColumn.DataPropertyName = "Код";
-            this.кодDataGridViewTextBoxColumn.HeaderText = "Код";
-            this.кодDataGridViewTextBoxColumn.Name = "кодDataGridViewTextBoxColumn";
-            this.кодDataGridViewTextBoxColumn.ReadOnly = true;
+            this.администраторToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.очиститьДиалогToolStripMenuItem});
+            this.администраторToolStripMenuItem.Name = "администраторToolStripMenuItem";
+            this.администраторToolStripMenuItem.Size = new System.Drawing.Size(106, 20);
+            this.администраторToolStripMenuItem.Text = "Администратор";
+            this.администраторToolStripMenuItem.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.администраторToolStripMenuItem.Visible = false;
             // 
-            // текстСообщенияDataGridViewTextBoxColumn
+            // очиститьДиалогToolStripMenuItem
             // 
-            this.текстСообщенияDataGridViewTextBoxColumn.DataPropertyName = "Текст сообщения";
-            this.текстСообщенияDataGridViewTextBoxColumn.HeaderText = "Текст сообщения";
-            this.текстСообщенияDataGridViewTextBoxColumn.Name = "текстСообщенияDataGridViewTextBoxColumn";
-            this.текстСообщенияDataGridViewTextBoxColumn.ReadOnly = true;
-            // 
-            // датаОтправкиDataGridViewTextBoxColumn
-            // 
-            this.датаОтправкиDataGridViewTextBoxColumn.DataPropertyName = "Дата отправки";
-            this.датаОтправкиDataGridViewTextBoxColumn.HeaderText = "Дата отправки";
-            this.датаОтправкиDataGridViewTextBoxColumn.Name = "датаОтправкиDataGridViewTextBoxColumn";
-            this.датаОтправкиDataGridViewTextBoxColumn.ReadOnly = true;
+            this.очиститьДиалогToolStripMenuItem.Name = "очиститьДиалогToolStripMenuItem";
+            this.очиститьДиалогToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.очиститьДиалогToolStripMenuItem.Text = "Очистить диалог";
+            this.очиститьДиалогToolStripMenuItem.Click += new System.EventHandler(this.ОчиститьДиалогToolStripMenuItem_Click);
             // 
             // Form1
             // 
@@ -432,14 +429,14 @@
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.Black;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(434, 685);
+            this.ClientSize = new System.Drawing.Size(1029, 428);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -504,9 +501,8 @@
         private KANonDBDataSet1TableAdapters.TableAdapterManager tableAdapterManager;
         private KANonDBDataSetTableAdapters.СообщенияTableAdapter сообщенияTableAdapter2;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn кодDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn текстСообщенияDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn датаОтправкиDataGridViewTextBoxColumn;
+        private System.Windows.Forms.ToolStripMenuItem администраторToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьДиалогToolStripMenuItem;
     }
 }
 
--- KANon/KANon/Form1.cs
+++ KANon/KANon/Form1.cs
@@ -10,63 +10,141 @@ using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.Data.OleDb;
 using System.Data.SqlClient;
+using System.Net;
 
 namespace KANon
 {
     public partial class Form1 : Form
     {
-        DateTime localDate = DateTime.Now;
-        string connectionString = ConfigurationManager.ConnectionStrings["MS_Access_DatabaseConnectionString"].ConnectionString;
-        OleDbConnection connection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\Denis\\Desktop\\Проект\\КАНон\\KANon\\KANon\\KANonDB.accdb");
-        public Form1()
+        //Запросы
+        string sqlRead = "SELECT Mails.Texts, Mails.Dates FROM Mails ORDER BY Mails.Dates DESC";
+        
+        
 
+
+
+        //Публичные переменные
+        private OleDbDataAdapter adapter = null;
+        private DataTable table = null;
+        private OleDbDataAdapter adapter2 = null;
+        private DataTable table2 = null;
+        DateTime localDate = DateTime.Now;
+        OleDbConnection connection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\Users\\dds036\\Desktop\\КАНон\\KANon\\KANon\\KANonDB.accdb");
+        int touchrows;
+
+
+
+        public Form1()
         {
-            
+
             InitializeComponent();
 
         }
 
-       private void Form1_Load(object sender, EventArgs e)
-       {
-            // TODO: данная строка кода позволяет загрузить данные в таблицу "kANonDBDataSet11.Mails". При необходимости она может быть перемещена или удалена.
-            //this.mailsTableAdapter.Fill(this.kANonDBDataSet11.Mails);
-
-            //textBox1.Text = "Write here...";
-            //textBox1.ForeColor = Color.Gray;
+        private void Form1_Load(object sender, EventArgs e)
+        {
             ToolTip tTip = new ToolTip();
+            string host = System.Net.Dns.GetHostName();
+            string ip = System.Net.Dns.GetHostByName(host).AddressList[0].ToString();
+
+
+
+            //
+            readfromBD();
+            //
+
+
+
+            OleDbCommand commandQ = new OleDbCommand();
+            commandQ.Connection = connection;
+            commandQ.CommandText = "SELECT Count(*) FROM Users where pc = '" + ip + "' ";
+            OleDbDataReader comRead;
+            if ((Int32)commandQ.ExecuteScalar()>0)
+            {
+               // OleDbCommand commandQ1 = new OleDbCommand();
+                commandQ.Connection = connection;
+                commandQ.CommandText = "SELECT admin FROM Users where pc = '" + ip + "' ";
+                comRead = commandQ.ExecuteReader();
+                comRead.Read();
+                if ((Boolean)comRead[0] == true)
+                {
+                    администраторToolStripMenuItem.Visible = true;
+                };
+               
+                comRead.Close();
+
+            }
+            else
+            {
+                OleDbCommand command1 = new OleDbCommand();
+                command1.Connection = connection;
+                command1.CommandText = "insert into Users (PC) values ('"+ ip +"')";
+                command1.ExecuteNonQuery();
+    
+            }
+
+           // touchrows = (int)commandQ.ExecuteScalar(); //Скаляр считает сколько строк было затронуто или нет??
+           // MessageBox.Show(Convert.ToString(touchrows));
+
+
+            
+
+
+
+            //OleDbDataAdapter dataAdapter = new OleDbDataAdapter(sqlRead, connection);
+            //DataSet ds = new DataSet();
+            //dataAdapter.Fill(ds, "[Miles]");
+            //dataGridView1.DataSource = ds.Tables[0].DefaultView;
 
             tTip.SetToolTip(this.button1, "Отправить сообщение");
             tTip.SetToolTip(this.button2, "Приложить файл (в последующих обновлениях)");
             tTip.SetToolTip(this.textBox1, "Write here...");
-            mailsBindingSource.DataSource = GetData("Select * From Mails order by id DESC");
-            dataGridView1.DataSource = mailsBindingSource;
 
+            
+        }
+        public void readfromBD()
+        {
+            adapter = new OleDbDataAdapter(sqlRead, connection);
+            DataTable ds = new DataTable();
+            if (connection.State == ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+         
+            adapter.Fill(ds);
+            dataGridView1.DataSource = ds;
 
         }
+        // public void GetData();
+
+
+
         //private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
         //{
         //textBox1.Text = null;
         //textBox1.ForeColor = Color.Black;
         //}
 
-        public DataTable GetData(string sqlCommand) 
+        public void GetDataFromMiles()
         {
 
-            if (connection.State == ConnectionState.Closed)
+            try
             {
-                connection.Open();
+                if (connection.State == ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+
             }
+            catch
+            {
+                MessageBox.Show("Ошибка соединения с базой");
+            }
+            string sql = "SELECT Mails.Texts, Mails.Dates FROM Mails ORDER BY Mails.Dates DESC";
+            OleDbCommand command = new OleDbCommand();
+            command.Connection = connection;
+            command.CommandText = sql;
 
-            SqlConnection northwindConnection = new SqlConnection(connectionString);
-
-            SqlCommand command = new SqlCommand(sqlCommand, northwindConnection);
-            SqlDataAdapter adapter = new SqlDataAdapter();
-            adapter.SelectCommand = command;
-
-            DataTable table = new DataTable();
-            table.Locale = System.Globalization.CultureInfo.InvariantCulture;
-            adapter.Fill(table);
-            return table;
         }
 
         private void ЖелтыйToolStripMenuItem1_Click(object sender, EventArgs e)
@@ -119,44 +197,36 @@ namespace KANon
                 command.Connection = connection;
                 command.CommandText = sql;
                 command.Parameters.AddWithValue("@Texts", textBox1.Text);
-                
-                
-                MessageBox.Show("Добавили " + command.ExecuteNonQuery() + " запись");
+                command.ExecuteNonQuery();
+
+                //MessageBox.Show("Добавили " + command.ExecuteNonQuery() + " запись");
             }
         }
         private void Button1_Click(object sender, EventArgs e)
         {
             InsertRow();
-            //DateTime localDate = DateTime.Now;
-            //using (OleDbConnection connection = new OleDbConnection(connectionString))
-            //{
-
-            //    try
-            //    {
-            //        if (connection.State == ConnectionState.Closed)
-            //        {
-            //            connection.Open();
-            //        }
-
-            //    }
-            //    catch
-            //    {
-            //        MessageBox.Show("Ошибка соединения с базой");
-            //    }
-            //    string sql = "insert into mails (Texts, Dates) values (@Texts, @Dates)";
-            //    OleDbCommand command = new OleDbCommand();
-            //    command.Connection = connection;
-            //    command.CommandText = sql;
-            //    command.Parameters.AddWithValue("@Texts", textBox1.Text);
-            //    command.Parameters.AddWithValue("@Dates", localDate);
-            //    int rows = command.ExecuteNonQuery();
-            //    //MessageBox.Show("Добавили " + rows.ToString() + " запись");
-            //}
+            readfromBD();
         }
 
         private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
+        }
+
+        private void ОчиститьДиалогToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+           
+            if (MessageBox.Show ("Вы уверены?", "Внимание", MessageBoxButtons.YesNo)== DialogResult.Yes)
+            {
+                OleDbCommand command = new OleDbCommand();
+                command.Connection = connection;
+                command.CommandText = "delete * from mails";
+                command.ExecuteNonQuery();
+                readfromBD();
+
+
+            }
+            
         }
     }
 }
